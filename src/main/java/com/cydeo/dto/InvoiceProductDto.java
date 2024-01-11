45516a985960f69bb64a3e64150bfeca5b546c1c
LENOVO <osmnozdmrosh@gmail.com>
2024-01-11 12:44:22 +0300

    EUAC2-11 - Fields are added in InvoiceProductDto

--- src/main/java/com/cydeo/dto/InvoiceProductDto.java
+++ src/main/java/com/cydeo/dto/InvoiceProductDto.java
@@ -1,4 +1,24 @@
 package com.cydeo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class InvoiceProductDto {
+
+    Long id;
+    Integer quantity;
+    BigDecimal price;
+    Integer tax;
+    BigDecimal total;
+    BigDecimal profitLoss;
+    Integer remainingQuantity;
+    InvoiceDto invoice;
+    ProductDto product;
 }
