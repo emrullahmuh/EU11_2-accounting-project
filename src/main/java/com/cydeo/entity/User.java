83d371bd1c9232eba8d4000887f7a76e061dd317
Orhan Isik <isiko979@gmail.com>
2024-01-11 13:01:20 +0300

    User entity created

--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -1,9 +1,34 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
+@Table(name = "users")
 public class User extends BaseEntity {
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String phone;
+    private boolean enabled;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
 }
