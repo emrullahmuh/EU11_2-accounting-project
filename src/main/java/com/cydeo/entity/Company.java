4af4a5856ca1e8c4c0114ecf0d218a51746f3a4a
selimdemirer <selimmdemirerr@gmail.com>
2024-01-11 18:14:31 +0300

    EUAC2-4: CompanyStatus enum, Company entity class and CompanyDto class are created

--- src/main/java/com/cydeo/entity/Company.java
+++ src/main/java/com/cydeo/entity/Company.java
@@ -1,9 +1,34 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import com.cydeo.enums.CompanyStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "companies")
 public class Company extends BaseEntity {
+
+    @Column(unique = true)
+    private String title;
+
+    private String phone;
+
+    private String website;
+
+    @Enumerated(EnumType.STRING)
+    private CompanyStatus companyStatus;
+
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
 }
--- src/main/resources/data1.sql
+++ src/main/resources/data1.sql
@@ -23,18 +23,18 @@ values ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Root User'),
 --         'SouthWest Street', 'Puzzle Square', 'Los Angeles', 'California', 'United States', '65654-8989'),
 --        ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
 --         'OwerWest Street', 'Android Square', 'Los Angeles', 'Phoneix', 'United States', '65654-8989');;
---
--- insert into companies(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
---                       title, phone, website, address_id, company_status)
--- values ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
---         'CYDEO','+1 (652) 852-8888', 'https://www.cydeo.com', 1, 'ACTIVE'),
---        ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
---         'Green Tech','+1 (652) 852-3246', 'https://www.greentech.com', 2, 'ACTIVE'),
---        ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
---         'Blue Tech','+1 (215) 654-5268', 'https://www.bluetech.com', 3, 'ACTIVE'),
---        ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
---         'Red Tech','+1 (215) 846-2642', 'https://www.redtech.com', 4, 'PASSIVE');
---
+
+insert into companies(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
+                      title, phone, website, address_id, company_status)
+values ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
+        'CYDEO','+1 (652) 852-8888', 'https://www.cydeo.com', 1, 'ACTIVE'),
+       ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
+        'Green Tech','+1 (652) 852-3246', 'https://www.greentech.com', 2, 'ACTIVE'),
+       ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
+        'Blue Tech','+1 (215) 654-5268', 'https://www.bluetech.com', 3, 'ACTIVE'),
+       ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
+        'Red Tech','+1 (215) 846-2642', 'https://www.redtech.com', 4, 'PASSIVE');
+
 -- insert into users(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
 --                   username, password, firstname, lastname, phone, role_id, company_id, enabled)
 -- values
