5193156957ddc4ccb33a3cc99dd434b4afb17226
yildirimsamet <yildirimsamet1@gmail.com>
2024-01-11 16:57:34 +0300

    EUAC2-9 ProductDTO class is created

--- src/main/java/com/cydeo/dto/ProductDto.java
+++ src/main/java/com/cydeo/dto/ProductDto.java
@@ -1,4 +1,20 @@
 package com.cydeo.dto;
 
+import com.cydeo.enums.ProductUnit;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDto {
+
+    private Long id;
+    private String name;
+    private Integer quantityInStock;
+    private Integer lowLimitAlert;
+    private ProductUnit productUnit;
+    private CategoryDto category;
+    private boolean hasProduct;
 }
