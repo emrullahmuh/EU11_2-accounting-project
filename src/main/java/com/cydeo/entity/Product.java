28101ca1092f547cd08b960e17824098bdad1551
yildirimsamet <yildirimsamet1@gmail.com>
2024-01-11 16:57:21 +0300

    EUAC2-9 Product class is created

--- src/main/java/com/cydeo/entity/Product.java
+++ src/main/java/com/cydeo/entity/Product.java
@@ -1,9 +1,28 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import com.cydeo.enums.ProductUnit;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
+@Table(name = "products")
 public class Product extends BaseEntity {
+
+    private String name;
+    private int quantityInStock;
+    private int lowLimitAlert;
+
+    @Enumerated(EnumType.STRING)
+    private ProductUnit productUnit;
+
+    @ManyToOne
+    @JoinColumn(name = "")
+    private Category category;
 }
