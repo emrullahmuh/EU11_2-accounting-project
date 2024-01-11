6c8fb02b8bb71a70f62572b914c18b57cc688742
cuneyt <cuneytky@gmail.com>
2024-01-12 00:13:02 +0300

    create Address and AddressDto

--- src/main/java/com/cydeo/entity/Address.java
+++ src/main/java/com/cydeo/entity/Address.java
@@ -1,9 +1,28 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
+
 
 @Entity
+@Table(name = "addresses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address extends BaseEntity {
+
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String country;
+    private String zipCode;
+
 }
