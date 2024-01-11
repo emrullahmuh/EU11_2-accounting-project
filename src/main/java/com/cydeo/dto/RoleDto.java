80cdcf65aa399d988c951b6491a86687dabc3114
Necibe <necibeablimit@hotmail.com>
2024-01-11 15:40:04 +0300

    Add Role entity & dto

--- src/main/java/com/cydeo/dto/RoleDto.java
+++ src/main/java/com/cydeo/dto/RoleDto.java
@@ -1,4 +1,15 @@
 package com.cydeo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class RoleDto {
+    private Long id;
+    private String description;
 }
--- src/main/java/com/cydeo/entity/Role.java
+++ src/main/java/com/cydeo/entity/Role.java
@@ -1,9 +1,21 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
+@Table(name = "roles")
 public class Role extends BaseEntity {
+    private String description;
+
 }
