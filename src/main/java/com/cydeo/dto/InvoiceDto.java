20c0067d25f059958fbe9a4f5464b13a81efc79f
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2024-01-11 13:23:18 +0300

    Dto entity and enums created

--- src/main/java/com/cydeo/dto/InvoiceDto.java
+++ src/main/java/com/cydeo/dto/InvoiceDto.java
@@ -1,4 +1,26 @@
 package com.cydeo.dto;
+import com.cydeo.enums.InvoiceStatus;
+import com.cydeo.enums.InvoiceType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class InvoiceDto {
+
+    private Long id;
+    private String invoiceNo;
+    private InvoiceStatus invoiceStatus;
+    private InvoiceType invoiceType;
+    private LocalDate date;
+    private CompanyDto company;
+    private ClientVendorDto clientVendor;
+    private BigDecimal price;
+    private BigDecimal tax;
+    private BigDecimal total;
 }
--- src/main/java/com/cydeo/entity/Invoice.java
+++ src/main/java/com/cydeo/entity/Invoice.java
@@ -1,9 +1,41 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import com.cydeo.enums.InvoiceStatus;
+import com.cydeo.enums.InvoiceType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-
+import javax.persistence.*;
+import java.time.LocalDate;
+//todo I need to add lombok I think
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
+@Table(name = "invoices")
 public class Invoice extends BaseEntity {
+
+    @Column(name = "invoice_no")
+    private String invoiceNo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "invoice_status")
+    private InvoiceStatus invoiceStatus;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "invoice_type")
+    private InvoiceType invoiceType;
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "client_vendor_id")
+    private ClientVendor clientVendor;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
 }
