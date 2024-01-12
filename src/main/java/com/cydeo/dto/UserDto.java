ffed58ff2cdf811a225d73b1a0e0af210c79d24f
Orhan Isik <isiko979@gmail.com>
2024-01-12 20:09:22 +0300

    @Data added to User entity  dto, @Builder added to UserDto

--- src/main/java/com/cydeo/dto/UserDto.java
+++ src/main/java/com/cydeo/dto/UserDto.java
@@ -1,14 +1,11 @@
 package com.cydeo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UserDto {
 
     private Long id;
--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -1,17 +1,13 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
+@Data
 @Entity
 @Table(name = "users")
 public class User extends BaseEntity {
