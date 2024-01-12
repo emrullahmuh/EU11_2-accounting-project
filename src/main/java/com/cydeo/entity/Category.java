8179dcb8ee54e1dcce299f1742b343aa715d908d
KupraEryılmaz <kupraeryilmaz@gmail.com>
2024-01-12 16:56:57 +0300

    EUAC2-8_Category is created

--- src/main/java/com/cydeo/entity/Category.java
+++ src/main/java/com/cydeo/entity/Category.java
@@ -1,9 +1,23 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
+@Table(name = "categories")
 public class Category extends BaseEntity {
+
+    private String description;
+    @ManyToOne
+    private Company company;
 }
