f3f6a9c2788ed109fc24affa98a93a03ba6bffa0
MertRam <mert.ramadan35@gmail.com>
2024-01-12 11:22:41 +0300

    ClientVendorType.java enum added and db password and name changed

---
+++ src/main/java/com/cydeo/enums/ClientVendorType.java
@@ -0,0 +1,18 @@
+package com.cydeo.enums;
+
+public enum ClientVendorType {
+    VENDOR("Vendor"),
+    CLIENT("Client");
+
+    private final String value;
+
+    ClientVendorType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+
+}
