16fcfdf537727f32c7dc52ef54e0c67318ff0a69
KupraEryılmaz <kupraeryilmaz@gmail.com>
2024-01-12 16:57:17 +0300

    EUAC2-8_CategoryDTO is created

--- src/main/java/com/cydeo/dto/CategoryDto.java
+++ src/main/java/com/cydeo/dto/CategoryDto.java
@@ -1,4 +1,16 @@
 package com.cydeo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryDto {
+
+    private Long id;
+    private String description;
+    private String company;
+    private boolean hasProduct;
 }
