0295dc552431605be2fa99da67326741803fc1bb
OsmanOzdemr <osmnozdmrosh@gmail.com>
2024-01-13 21:27:37 +0300

    EUAC2-13 - Add SecurityService and SecurityServiceImpl

---
+++ src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
@@ -0,0 +1,30 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.entity.User;
+import com.cydeo.entity.common.UserPrincipal;
+import com.cydeo.repository.UserRepository;
+import com.cydeo.service.SecurityService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityServiceImpl implements SecurityService {
+
+    private final UserRepository userRepository;
+
+    public SecurityServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+       User user = userRepository.findByUsernameAndIsDeleted(username,false);
+
+       if(user==null){
+           throw new UsernameNotFoundException(username);
+       }
+       return new UserPrincipal(user);
+    }
+}
