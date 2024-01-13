c8df665c88b41f7a71af3656bcbe3ddf6f3cfaaa
OsmanOzdemr <osmnozdmrosh@gmail.com>
2024-01-13 21:28:53 +0300

    EUAC2-13 - Add AuthSuccessHandler Class

---
+++ src/main/java/com/cydeo/config/AuthSuccessHandler.java
@@ -0,0 +1,38 @@
+package com.cydeo.config;
+
+import org.springframework.security.core.Authentication;
+
+import org.springframework.security.core.authority.AuthorityUtils;
+
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import java.util.Set;
+
+public class AuthSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+
+        if(roles.contains("Root User")){
+            response.sendRedirect("/companies/list");
+        }
+
+        if(roles.contains("Admin")){
+            response.sendRedirect("/users/list");
+        }
+
+        if(roles.contains("Manager") || roles.contains("Employee")){
+            response.sendRedirect("/dashboard");
+        }
+
+    }
+}
