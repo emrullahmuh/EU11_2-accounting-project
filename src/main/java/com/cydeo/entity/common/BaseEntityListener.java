a95f42b347e8ea1c341c6cc65bd4c20e89ea46a5
OsmanOzdemr <osmnozdmrosh@gmail.com>
2024-01-13 21:30:12 +0300

    EUAC2-13 - Add BaseEntityListener Class

---
+++ src/main/java/com/cydeo/entity/common/BaseEntityListener.java
@@ -0,0 +1,40 @@
+package com.cydeo.entity.common;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
+public class BaseEntityListener extends AuditingEntityListener {
+
+    @PrePersist
+    private void onPrePersist(BaseEntity baseEntity){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        baseEntity.setInsertDateTime(LocalDateTime.now());
+        baseEntity.setLastUpdateDateTime(LocalDateTime.now());
+
+        if(authentication != null && !authentication.getName().equals("anonymousUser")){
+            Object principal = authentication.getPrincipal();
+            baseEntity.setInsertUserId(((UserPrincipal) principal).getId());
+            baseEntity.setLastUpdateUserId( ((UserPrincipal) principal).getId());
+        }
+    }
+
+    @PreUpdate
+    private void onPreUpdate(BaseEntity baseEntity){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        baseEntity.setLastUpdateDateTime(LocalDateTime.now());
+
+        if(authentication != null && !authentication.getName().equals("anonymousUser")){
+            Object principal = authentication.getPrincipal();
+            baseEntity.setLastUpdateUserId( ((UserPrincipal) principal).getId());
+        }
+    }
+}
