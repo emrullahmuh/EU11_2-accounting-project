91eaf74eca95c9e00e843024390741bb358eb6ea
OsmanOzdemr <osmnozdmrosh@gmail.com>
2024-01-13 17:53:06 +0300

    EUAC2-13 - Add getId() Method

--- src/main/java/com/cydeo/entity/common/UserPrincipal.java
+++ src/main/java/com/cydeo/entity/common/UserPrincipal.java
@@ -57,4 +57,9 @@ public class UserPrincipal implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public Long getId(){
+        return this.user.getId();
+    }
+
 }
