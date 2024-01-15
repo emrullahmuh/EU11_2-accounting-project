7f6c8d83afda74ef191a690c204513c899c4f983
OsmanOzdemr <osmnozdmrosh@gmail.com>
2024-01-15 09:09:32 +0300

    EUAC2-13 - Add @Configuration annotation

--- src/main/java/com/cydeo/config/AuthSuccessHandler.java
+++ src/main/java/com/cydeo/config/AuthSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.cydeo.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -15,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import java.util.Set;
-
+@Configuration
 public class AuthSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
