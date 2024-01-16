54883fb5e74b9fe9b6ca066736ed2f8df5e49a70
OsmanOzdemr <osmnozdmrosh@gmail.com>
2024-01-16 16:41:23 +0300

    EUAC2-18 - Create custom CategoryNotFoundException Class

---
+++ src/main/java/com/cydeo/exception/CategoryNotFoundException.java
@@ -0,0 +1,8 @@
+package com.cydeo.exception;
+
+public class CategoryNotFoundException extends Exception{
+
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
+}
