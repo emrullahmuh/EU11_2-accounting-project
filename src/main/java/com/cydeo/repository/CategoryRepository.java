76b680592a21a67d262b9f19f6863ed416576529
OsmanOzdemr <osmnozdmrosh@gmail.com>
2024-01-16 16:42:03 +0300

    EUAC2-18 - Update CategoryRepository

--- src/main/java/com/cydeo/repository/CategoryRepository.java
+++ src/main/java/com/cydeo/repository/CategoryRepository.java
@@ -4,11 +4,12 @@ import com.cydeo.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface CategoryRepository extends JpaRepository<Category,Long> {
 
-    Category findByIdAndIsDeleted(Long id, boolean isDeleted);
+    Optional<Category> findByIdAndIsDeleted(Long id, boolean isDeleted);
 
     List<Category> findAllByIsDeleted(boolean isDeleted);
 }
