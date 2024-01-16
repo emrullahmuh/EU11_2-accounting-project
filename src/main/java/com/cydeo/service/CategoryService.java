ab0cea67c457c0451028b99c93e07bf7bb50bc5d
OsmanOzdemr <osmnozdmrosh@gmail.com>
2024-01-16 09:27:31 +0300

    EUAC2-18 - Update CategoryService and CategoryServiceImpl

--- src/main/java/com/cydeo/service/CategoryService.java
+++ src/main/java/com/cydeo/service/CategoryService.java
@@ -1,4 +1,11 @@
 package com.cydeo.service;
 
+import com.cydeo.dto.CategoryDto;
+
+import java.util.List;
+
 public interface CategoryService {
+
+    CategoryDto getById(Long id);
+    List<CategoryDto> listAllCategories();
 }
--- src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,40 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.dto.CategoryDto;
+import com.cydeo.entity.Category;
+import com.cydeo.repository.CategoryRepository;
 import com.cydeo.service.CategoryService;
+import com.cydeo.util.MapperUtil;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final MapperUtil mapperUtil;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository, MapperUtil mapperUtil) {
+        this.categoryRepository = categoryRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
+    @Override
+    public CategoryDto getById(Long id) {
+
+       Optional<Category> category = categoryRepository.findById(id);
+        return mapperUtil.convert(category,new CategoryDto()) ;
+    }
+
+    @Override
+    public List<CategoryDto> listAllCategories() {
+
+        List<Category> categoryList = categoryRepository.findAll();
+        return categoryList.stream()
+                .map(category->mapperUtil.convert(category, new CategoryDto()))
+                .collect(Collectors.toList());
+    }
 }
