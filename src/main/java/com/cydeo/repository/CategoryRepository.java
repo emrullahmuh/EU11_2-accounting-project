188c6dcc9931ed136d0d1aa6b7a5701bc99b8130
OsmanOzdemr <osmnozdmrosh@gmail.com>
2024-01-16 14:58:33 +0300

    EUAC2-18 - Add methods to CategoryRepository

--- src/main/java/com/cydeo/repository/CategoryRepository.java
+++ src/main/java/com/cydeo/repository/CategoryRepository.java
@@ -3,5 +3,12 @@ package com.cydeo.repository;
 import com.cydeo.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+
 public interface CategoryRepository extends JpaRepository<Category,Long> {
+
+    Category findByIdAndIsDeleted(Long id, boolean isDeleted);
+
+    List<Category> findAllByIsDeleted(boolean isDeleted);
 }
