8f294e099f76bac3974ddbb83c6e8a4f855576f3
OsmanOzdemr <osmnozdmrosh@gmail.com>
2024-01-19 18:09:57 +0300

    EUAC2-18 - Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,28 +1,28 @@
---insert into roles(insert_date_time, insert_user_id, is_deleted, last_update_date_time,
---                 last_update_user_id, description)
---values ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Root User'),
---      ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Admin'),
---       ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Manager'),
---       ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Employee');
+insert into roles(insert_date_time, insert_user_id, is_deleted, last_update_date_time,
+                last_update_user_id, description)
+values ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Root User'),
+     ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Admin'),
+      ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Manager'),
+      ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Employee');
 
--- insert into addresses(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
---                       address_line1, address_line2, city, state, country, zip_code)
--- values ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
---         '7925 Jones Branch Dr, #3300', 'Tysons', 'Virginia', 'VA', 'United States', '22102-1234'),
---        ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
---         'Future Street', 'Times Square', 'Atlanta', 'Alabama', 'United States', '54321-4321'),
---        ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
---         'North Street', 'Circle Square', 'San Francisco', 'California', 'United States', '65245-8546'),
---        ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
---         'West Street', 'Triangle Square', 'Los Angeles', 'California', 'United States', '54782-5214'),
---        ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
---         'East Street', 'Cube Square', 'Los Angeles', 'California', 'United States', '54782-5214'),
---        ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
---         'South Street', 'Times Square', 'Los Angeles', 'California', 'United States', '54782-5214'),
---        ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
---         'SouthWest Street', 'Puzzle Square', 'Los Angeles', 'California', 'United States', '65654-8989'),
---        ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
---         'OwerWest Street', 'Android Square', 'Los Angeles', 'Phoneix', 'United States', '65654-8989');;
+insert into addresses(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
+                      address_line1, address_line2, city, state, country, zip_code)
+values ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
+        '7925 Jones Branch Dr, #3300', 'Tysons', 'Virginia', 'VA', 'United States', '22102-1234'),
+       ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
+        'Future Street', 'Times Square', 'Atlanta', 'Alabama', 'United States', '54321-4321'),
+       ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
+        'North Street', 'Circle Square', 'San Francisco', 'California', 'United States', '65245-8546'),
+       ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
+        'West Street', 'Triangle Square', 'Los Angeles', 'California', 'United States', '54782-5214'),
+       ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
+        'East Street', 'Cube Square', 'Los Angeles', 'California', 'United States', '54782-5214'),
+       ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
+        'South Street', 'Times Square', 'Los Angeles', 'California', 'United States', '54782-5214'),
+       ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
+        'SouthWest Street', 'Puzzle Square', 'Los Angeles', 'California', 'United States', '65654-8989'),
+       ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
+        'OwerWest Street', 'Android Square', 'Los Angeles', 'Phoneix', 'United States', '65654-8989');;
 
 insert into companies(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
                       title, phone, website, address_id, company_status)
@@ -35,42 +35,42 @@ values ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
        ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
         'Red Tech','+1 (215) 846-2642', 'https://www.redtech.com', 4, 'PASSIVE');
         
--- insert into users(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
---                   username, password, firstname, lastname, phone, role_id, company_id, enabled)
--- values
--- -- COMPANY-1 / CYDEO / ROOT USER
--- ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
---  'root@cydeo.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
---  'Robert', 'Martin', '+1 (852) 564-5874', 1, 1, true),
--- -- COMPANY-2 / Green Tech / ADMIN-1
--- ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
---  'admin@greentech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
---  'Mary', 'Grant', '+1 (234) 345-4362', 2, 2, true),
--- -- COMPANY-2 / Green Tech / ADMIN-2
--- ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
---  'admin2@greentech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
---  'Garrison', 'Short', '+1 (234) 356-7865', 2, 2, true),
--- -- COMPANY-2 / Green Tech / MANAGER
--- ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
---  'manager@greentech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
---  'Robert', 'Noah', '+1 (234) 564-5874', 3, 2, true),
--- -- COMPANY-2 / Green Tech / EMPLOYEE
--- ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
---  'employee@greentech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
---  'Mike', 'Times', '+1 (234) 741-8569', 4, 2, true),
--- -- COMPANY-3 / Blue Tech / ADMIN
--- ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
---  'admin@bluetech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
---  'Chris', 'Brown', '+1 (356) 258-3544', 2, 3, true),
--- -- COMPANY-3 / Blue Tech / MANAGER
--- ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
---  'manager@bluetech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
---  'Tom', 'Hanks', '+1 (356) 258-3544', 3, 3, true),
--- -- COMPANY-4 / Red Tech / ADMIN
--- ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
---  'admin@redtech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
---  'John', 'Doe', '+1 (659) 756-1265', 2, 4, true);
---
+insert into users(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
+                  username, password, firstname, lastname, phone, role_id, company_id, enabled)
+values
+-- COMPANY-1 / CYDEO / ROOT USER
+('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
+ 'root@cydeo.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
+ 'Robert', 'Martin', '+1 (852) 564-5874', 1, 1, true),
+-- COMPANY-2 / Green Tech / ADMIN-1
+('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
+ 'admin@greentech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
+ 'Mary', 'Grant', '+1 (234) 345-4362', 2, 2, true),
+-- COMPANY-2 / Green Tech / ADMIN-2
+('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
+ 'admin2@greentech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
+ 'Garrison', 'Short', '+1 (234) 356-7865', 2, 2, true),
+-- COMPANY-2 / Green Tech / MANAGER
+('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
+ 'manager@greentech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
+ 'Robert', 'Noah', '+1 (234) 564-5874', 3, 2, true),
+-- COMPANY-2 / Green Tech / EMPLOYEE
+('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
+ 'employee@greentech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
+ 'Mike', 'Times', '+1 (234) 741-8569', 4, 2, true),
+-- COMPANY-3 / Blue Tech / ADMIN
+('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
+ 'admin@bluetech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
+ 'Chris', 'Brown', '+1 (356) 258-3544', 2, 3, true),
+-- COMPANY-3 / Blue Tech / MANAGER
+('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
+ 'manager@bluetech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
+ 'Tom', 'Hanks', '+1 (356) 258-3544', 3, 3, true),
+-- COMPANY-4 / Red Tech / ADMIN
+('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
+ 'admin@redtech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
+ 'John', 'Doe', '+1 (659) 756-1265', 2, 4, true);
+
 insert into clients_vendors(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
                              client_vendor_type, client_vendor_name, phone, website, address_id, company_id)
  values
@@ -109,27 +109,27 @@ insert into products(insert_date_time, insert_user_id, is_deleted, last_update_d
     ('2022-09-15 00:00', 3, 'false', '2022-09-15 00:00', 3, 'Samsung Galaxy S20 (renewed)', 30, 5, 'PCS', 3),
     ('2022-09-15 00:00', 3, 'false', '2022-09-15 00:00', 3, 'Samsung Galaxy S22', 20, 5, 'PCS', 3),
     ('2022-09-15 00:00', 3, 'false', '2022-09-15 00:00', 3, 'Moto G Power', 0, 5, 'PCS', 3);
---
---
--- insert into invoices(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
---                      date,invoice_no, invoice_type, invoice_status, client_vendor_id, company_id)
--- values
--- -- COMPANY-2 / Green Tech
---  ('2022-09-09 00:00', 2, 'false', '2022-09-09 00:00', 2, '2022-09-09', 'P-001', 'PURCHASE', 'APPROVED', 2, 2),
---  ('2022-09-10 00:00', 2, 'false', '2022-09-10 00:00', 2, '2022-09-10', 'P-002', 'PURCHASE', 'APPROVED', 3, 2),
---  ('2022-09-17 00:00', 2, 'false', '2022-09-17 00:00', 2, '2022-09-17', 'S-001', 'SALES', 'APPROVED', 1, 2),
---  ('2022-10-19 00:00', 2, 'false', '2022-10-19 00:00', 2, '2022-10-19', 'S-002', 'SALES', 'AWAITING_APPROVAL', 1, 2),
---  ('2022-11-20 00:00', 2, 'false', '2022-11-20 00:00', 2, '2022-11-20', 'S-003', 'SALES', 'AWAITING_APPROVAL', 1, 2),
---
--- -- COMPANY-3 / Blue Tech
--- ('2022-09-09 00:00', 3, 'false', '2022-09-09 00:00', 3, '2022-09-09', 'P-001', 'PURCHASE', 'APPROVED', 5, 3),
--- ('2022-09-10 00:00', 3, 'false', '2022-09-10 00:00', 3, '2022-09-10', 'P-002', 'PURCHASE', 'APPROVED', 5, 3),
--- ('2022-09-13 00:00', 3, 'false', '2022-09-13 00:00', 3, '2022-09-13', 'S-001', 'SALES', 'APPROVED', 4, 3),
--- ('2022-11-18 00:00', 3, 'false', '2022-11-18 00:00', 3, '2022-11-18', 'S-002', 'SALES', 'AWAITING_APPROVAL', 4, 3),
--- ('2022-11-19 00:00', 3, 'false', '2022-11-19 00:00', 3, '2022-11-19', 'S-003', 'SALES', 'AWAITING_APPROVAL', 4, 3),
--- ('2022-11-20 00:00', 3, 'false', '2022-11-20 00:00', 3, '2022-11-20', 'S-004', 'SALES', 'AWAITING_APPROVAL', 6, 3),
--- ('2022-11-21 00:00', 3, 'false', '2022-11-21 00:00', 3, '2022-11-21', 'S-005', 'SALES', 'AWAITING_APPROVAL', 6, 3),
--- ('2022-12-15 00:00', 3, 'false', '2022-12-15 00:00', 3, '2022-12-15', 'P-003', 'PURCHASE', 'AWAITING_APPROVAL', 6, 3);
+
+
+insert into invoices(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
+                     date,invoice_no, invoice_type, invoice_status, client_vendor_id, company_id)
+values
+-- COMPANY-2 / Green Tech
+ ('2022-09-09 00:00', 2, 'false', '2022-09-09 00:00', 2, '2022-09-09', 'P-001', 'PURCHASE', 'APPROVED', 2, 2),
+ ('2022-09-10 00:00', 2, 'false', '2022-09-10 00:00', 2, '2022-09-10', 'P-002', 'PURCHASE', 'APPROVED', 3, 2),
+ ('2022-09-17 00:00', 2, 'false', '2022-09-17 00:00', 2, '2022-09-17', 'S-001', 'SALES', 'APPROVED', 1, 2),
+ ('2022-10-19 00:00', 2, 'false', '2022-10-19 00:00', 2, '2022-10-19', 'S-002', 'SALES', 'AWAITING_APPROVAL', 1, 2),
+ ('2022-11-20 00:00', 2, 'false', '2022-11-20 00:00', 2, '2022-11-20', 'S-003', 'SALES', 'AWAITING_APPROVAL', 1, 2),
+
+-- COMPANY-3 / Blue Tech
+('2022-09-09 00:00', 3, 'false', '2022-09-09 00:00', 3, '2022-09-09', 'P-001', 'PURCHASE', 'APPROVED', 5, 3),
+('2022-09-10 00:00', 3, 'false', '2022-09-10 00:00', 3, '2022-09-10', 'P-002', 'PURCHASE', 'APPROVED', 5, 3),
+('2022-09-13 00:00', 3, 'false', '2022-09-13 00:00', 3, '2022-09-13', 'S-001', 'SALES', 'APPROVED', 4, 3),
+('2022-11-18 00:00', 3, 'false', '2022-11-18 00:00', 3, '2022-11-18', 'S-002', 'SALES', 'AWAITING_APPROVAL', 4, 3),
+('2022-11-19 00:00', 3, 'false', '2022-11-19 00:00', 3, '2022-11-19', 'S-003', 'SALES', 'AWAITING_APPROVAL', 4, 3),
+('2022-11-20 00:00', 3, 'false', '2022-11-20 00:00', 3, '2022-11-20', 'S-004', 'SALES', 'AWAITING_APPROVAL', 6, 3),
+('2022-11-21 00:00', 3, 'false', '2022-11-21 00:00', 3, '2022-11-21', 'S-005', 'SALES', 'AWAITING_APPROVAL', 6, 3),
+('2022-12-15 00:00', 3, 'false', '2022-12-15 00:00', 3, '2022-12-15', 'P-003', 'PURCHASE', 'AWAITING_APPROVAL', 6, 3);
 
 insert into invoice_products(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
                              price,quantity, remaining_quantity, tax, profit_loss, invoice_id, product_id)
