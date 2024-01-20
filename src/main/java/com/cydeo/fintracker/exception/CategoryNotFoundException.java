10fc32c413a9cf64334c8a480a1ecc13ef53dd42
OsmanOzdemr <osmnozdmrosh@gmail.com>
2024-01-20 12:28:02 +0300

    EUAC2-18 - Change exception type as unchecked exception

--- src/main/java/com/cydeo/fintracker/exception/CategoryNotFoundException.java
+++ src/main/java/com/cydeo/fintracker/exception/CategoryNotFoundException.java
@@ -1,6 +1,6 @@
 package com.cydeo.fintracker.exception;
 
-public class CategoryNotFoundException extends Exception{
+public class CategoryNotFoundException extends RuntimeException{
 
     public CategoryNotFoundException(String message) {
         super(message);
