1566aab3d4426dca032f10dc43f7fb00d41601d1
yildirimsamet <yildirimsamet1@gmail.com>
2024-01-21 12:26:43 +0300

    ProductRepository is fixed

--- src/main/java/com/cydeo/fintracker/repository/ProductRepository.java
+++ src/main/java/com/cydeo/fintracker/repository/ProductRepository.java
@@ -2,10 +2,14 @@ package com.cydeo.fintracker.repository;
 
 import com.cydeo.fintracker.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
+@Transactional
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
     List<Product> findAll();
