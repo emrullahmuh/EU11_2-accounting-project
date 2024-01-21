767ae3a4fa73b3f10df484fe4fcd3a611db294f0
yildirimsamet <yildirimsamet1@gmail.com>
2024-01-21 12:26:10 +0300

    controller class is fixed

--- src/main/java/com/cydeo/fintracker/controller/ProductController.java
+++ src/main/java/com/cydeo/fintracker/controller/ProductController.java
@@ -2,15 +2,11 @@ package com.cydeo.fintracker.controller;
 
 
 import com.cydeo.fintracker.dto.ProductDto;
-import com.cydeo.fintracker.entity.Company;
-import com.cydeo.fintracker.entity.Product;
-import com.cydeo.fintracker.entity.common.UserPrincipal;
 import com.cydeo.fintracker.enums.ProductUnit;
 import com.cydeo.fintracker.service.CategoryService;
 import com.cydeo.fintracker.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -33,15 +29,15 @@ public class ProductController {
     @GetMapping("/list")
     public String getProductList(Model model){
 
-        Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+//        Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+//
+//        UserPrincipal userPrincipal=(UserPrincipal) authentication.getPrincipal();
+//
+//        String userCompanyId=userPrincipal.getCompanyTitleForProfile();
+//
+//        List<Product> userProducts=productService.getProductsByCompanyId(userPrincipal.getId());
 
-        UserPrincipal userPrincipal=(UserPrincipal) authentication.getPrincipal();
-
-        String userCompanyId=userPrincipal.getCompanyTitleForProfile();
-
-        List<Product> userProducts=productService.getProductsByCompanyId(userPrincipal.getId());
-
-        model.addAttribute("products",userProducts);
+        model.addAttribute("products",productService.getProducts());
 
         return "product/product-list";
 
@@ -49,9 +45,9 @@ public class ProductController {
 
 
     @GetMapping("/update/{id}")
-    public String updateProduct(@PathVariable ("id") Long productId, Model model){
+    public String updateProduct(@PathVariable ("id") Long id, Model model){
 
-        model.addAttribute("product", productService.findById(productId));
+        model.addAttribute("product", productService.findById(id));
         model.addAttribute("categories",categoryService.listAllCategories());
         model.addAttribute("productUnits",ProductUnit.values());
 
@@ -66,7 +62,7 @@ public class ProductController {
         return "redirect:/products/list";
     }
 
-    @DeleteMapping("/delete/{id}")
+    @GetMapping("/delete/{id}")
     public String deleteProduct(@PathVariable ("id") Long id){
 
         productService.delete(id);
