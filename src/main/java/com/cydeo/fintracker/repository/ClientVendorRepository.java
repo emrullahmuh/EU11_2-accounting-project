b1e0de28ed63ce52660618403ffc1d93644289e9
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2024-01-22 23:52:11 +0300

    unit tests created and the method altered for the possibility of empty list.

--- src/main/java/com/cydeo/fintracker/repository/ClientVendorRepository.java
+++ src/main/java/com/cydeo/fintracker/repository/ClientVendorRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ClientVendorRepository extends JpaRepository <ClientVendor,Long>{
     ClientVendor findByClientVendorNameAndIsDeleted(String username, Boolean deleted);
-    List<ClientVendor> findAllByIsDeleted(Boolean deleted);
+    Optional<List<ClientVendor>> findAllByIsDeleted(Boolean deleted);
 }
