b1dda6587a4fe102f84556e5c639e68c4ca588ab
MertRam <mert.ramadan35@gmail.com>
2024-01-23 15:56:22 +0300

    EUAC2-33 Vlidation messages added and work properly at local machine

--- src/main/java/com/cydeo/fintracker/dto/AddressDto.java
+++ src/main/java/com/cydeo/fintracker/dto/AddressDto.java
@@ -1,6 +1,13 @@
 package com.cydeo.fintracker.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 
 @NoArgsConstructor
@@ -10,11 +17,28 @@ import lombok.*;
 public class AddressDto {
 
     private Long id;
+
+    @NotBlank(message = "Address is a required field.")
+    @Size(min = 2, max = 100, message = "Address should have 2-100 characters long.")
     private String addressLine1;
+
+    @Size(max = 100, message = "Address should have maximum 100 characters long.")
     private String addressLine2;
+
+    @NotBlank(message = "City is required field.")
+    @Size(min = 2, max = 50, message = "City should have 2-50 characters long.")
     private String city;
+
+    @NotBlank(message = "State is required field.")
+    @Size(min = 2, max = 50, message = "State should have 2-50 characters long.")
     private String state;
+
+    @NotBlank(message = "Country is a required field.")
+    @Size(min = 2, max = 50, message = "Country should have 2-50 characters long.")
     private String country;
+
+    @NotBlank(message = "Zipcode is a required field.")
+    @Pattern(regexp = "\\d{5}([-]\\d{4})?$", message = "Zipcode should have a valid form")
     private String zipCode;
 
 }
--- src/main/java/com/cydeo/fintracker/dto/ClientVendorDto.java
+++ src/main/java/com/cydeo/fintracker/dto/ClientVendorDto.java
@@ -6,6 +6,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,10 +19,22 @@ import lombok.NoArgsConstructor;
 public class ClientVendorDto {
 
     private Long id;
+
+    @NotBlank(message = "Company Name is required field")
+    @NotNull
+    @Size(min = 2, max = 100, message = "Company Name must be between 2 and 50 characters long.")
     private String clientVendorName;
+
+    @NotBlank(message = "Phone Number is required field and may be in any valid phone number format.")
     private String phone;
+
+    @NotBlank(message = "Website should have a valid format.")
+    @Pattern(regexp = "^http(s{0,1})://[a-zA-Z0-9/\\-\\.]+\\.([A-Za-z/]{2,5})[a-zA-Z0-9/\\&\\?\\=\\-\\.\\~\\%]*", message = "Website should have a valid format.")
     private String website;
+
     private ClientVendorType clientVendorType;
+
+    @Valid
     private AddressDto address;
     private CompanyDto company;
     private boolean hasInvoice;
