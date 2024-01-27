a97df19b3e4809b30902c74bab3ece99f85739e5
Necibe <necibeablimit@hotmail.com>
2024-01-27 15:36:38 +0300

    EUAC2-30: added isUserOnlyAdmin query in repository

--- src/main/java/com/cydeo/fintracker/repository/UserRepository.java
+++ src/main/java/com/cydeo/fintracker/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.cydeo.fintracker.repository;
 
+import com.cydeo.fintracker.entity.Company;
+import com.cydeo.fintracker.entity.Role;
 import com.cydeo.fintracker.entity.User;
 
+import feign.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,4 +19,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     List<User> findAllByIsDeleted(Boolean deleted);
+
+
+@Query("SELECT COUNT(u) FROM User u WHERE u.company = :company AND u.role.id = :roleId")
+Integer isUserOnlyAdmin(@Param("company") Company company, @Param("roleId") Long roleId);
+
 }
