8b34e56ba8ee3e92446674a5809c3e98944ba91c
yildirimsamet <yildirimsamet1@gmail.com>
2024-01-30 13:59:53 +0300

    EUAC2-42d - serviceimpl is fixed

--- src/main/java/com/cydeo/fintracker/service/impl/ProductServiceImpl.java
+++ src/main/java/com/cydeo/fintracker/service/impl/ProductServiceImpl.java
@@ -10,6 +10,7 @@ import com.cydeo.fintracker.entity.Category;
 import com.cydeo.fintracker.entity.Product;
 import com.cydeo.fintracker.repository.ProductRepository;
 import com.cydeo.fintracker.service.ProductService;
+import com.cydeo.fintracker.service.SecurityService;
 import com.cydeo.fintracker.util.MapperUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,32 +29,38 @@ public class ProductServiceImpl implements ProductService {
 
     private final MapperUtil mapperUtil;
     private final ProductRepository productRepository;
+    private final SecurityService securityService;
 
 
     @Override
     public List<ProductDto> getProducts() {
 
-        List<Product> products = productRepository.findAll();
+        Long loggedInUserId = securityService.getLoggedInUser().getId();
+
+
+        List<Product> products = productRepository.getProductsByInsertUserIdAndIsDeleted(loggedInUserId, false);
+
         return products.stream()
-                .map(product -> mapperUtil.convert(product, new ProductDto()))
-                .collect(Collectors.toList());
+                .map(product -> mapperUtil.convert(product, new ProductDto())).collect(Collectors.toList());
     }
 
     @Override
     public ProductDto updateProduct(ProductDto productDto) {
 
-        Optional<Product> oldProduct = productRepository.findById(productDto.getId());
+        Optional<Product> oldProductOptional = productRepository.findById(productDto.getId());
 
-        Product product = oldProduct.get();
+        Product product = mapperUtil.convert(productDto, new Product());
 
-        log.info("Product will be updated : '{}'", product);
+        product.setId(oldProductOptional.get().getId());
+        product.setQuantityInStock(oldProductOptional.get().getQuantityInStock());
 
-        Product newProduct = productRepository.save(mapperUtil.convert(productDto, new Product()));
+        Product savedProduct = productRepository.save(product);
+        log.info("Product will be updated : '{}'", savedProduct);
 
-        ProductDto updatedProduct = mapperUtil.convert(newProduct, productDto);
-        log.info("Product is updated '{}', '{}': ", updatedProduct.getName(), updatedProduct);
+        ProductDto savedProductDto = mapperUtil.convert(savedProduct, new ProductDto());
+        log.info("Product is updated '{}', '{}': ", savedProductDto.getName(), savedProductDto);
 
-        return updatedProduct;
+        return savedProductDto;
     }
 
     @Override
@@ -70,20 +77,15 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void delete(Long id) {
 
-        productRepository.deleteProductById(id);
+        Optional<Product> product = productRepository.findById(id);
+
+        product.get().setIsDeleted(true);
+        productRepository.save(product.get());
         log.info("Product is deleted '{}', '{}'", id, id);
 
     }
 
     @Override
-    public List<Product> getProductsByCompanyId(Long companyId) {
-
-        return productRepository.getProductsById(companyId);
-
-    }
-
-    @Override
-
     public List<ProductDto> getProductsByCategory(Long id) {
 
         List<Product> products = productRepository.findByCategory(id);
