e15a31dac8abfd18e17069f3689418fce90684ab
emrulla muhter <125178078+emrullahmuh@users.noreply.github.com>
2024-01-30 19:41:53 +0300

    Euac2 59  (#62)
    * EUAC2-59 report Controller created
    
    * EUAC2-59 report service and serviceimpl created
    
    ---------
    
    Co-authored-by: emrulla muhter <emrullauigur@gmai.com>

---
+++ src/main/java/com/cydeo/fintracker/controller/ReportingController.java
@@ -0,0 +1,12 @@
+package com.cydeo.fintracker.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/reports")
+public class ReportingController {
+}
