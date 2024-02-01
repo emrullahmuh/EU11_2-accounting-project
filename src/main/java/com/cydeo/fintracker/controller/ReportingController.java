521187f0962f3591f06f2bf9e0fd6c3c688efaff
emrulla muhter <emrullauigur@gmai.com>
2024-02-02 00:25:57 +0300

    EUAC2-61 added getProfitLossList method to controller

--- src/main/java/com/cydeo/fintracker/controller/ReportingController.java
+++ src/main/java/com/cydeo/fintracker/controller/ReportingController.java
@@ -1,12 +1,21 @@
 package com.cydeo.fintracker.controller;
 
+import com.cydeo.fintracker.service.ReportingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/reports")
 public class ReportingController {
+    private final ReportingService reportingService;
+    @GetMapping("/profitLossData")
+    public String  getProfitLossList(Model model){
+        model.addAttribute("monthlyProfitLossDataMap",reportingService.getMonthlyProfitLoss);
+    return "report/profit-loss-report";
+    }
 }
