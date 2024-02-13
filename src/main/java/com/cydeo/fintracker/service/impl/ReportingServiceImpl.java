8540f37525e516f7b59e488dcee8efba283be92f
emrulla muhter <emrullauigur@gmai.com>
2024-02-13 18:17:38 +0300

    EUAC2-61 - updated ReportingServiceImpl

--- src/main/java/com/cydeo/fintracker/service/impl/ReportingServiceImpl.java
+++ src/main/java/com/cydeo/fintracker/service/impl/ReportingServiceImpl.java
@@ -1,28 +1,20 @@
 package com.cydeo.fintracker.service.impl;
 
 import com.cydeo.fintracker.dto.CompanyDto;
-import com.cydeo.fintracker.entity.Company;
 import com.cydeo.fintracker.enums.InvoiceType;
+import com.cydeo.fintracker.repository.ReportRepository;
 import com.cydeo.fintracker.service.*;
-import com.cydeo.fintracker.util.MapperUtil;
 import com.cydeo.fintracker.dto.InvoiceProductDto;
-import com.cydeo.fintracker.entity.Invoice;
-import com.cydeo.fintracker.entity.InvoiceProduct;
 import com.cydeo.fintracker.enums.InvoiceStatus;
 import com.cydeo.fintracker.service.InvoiceProductService;
-import com.cydeo.fintracker.service.InvoiceService;
 import com.cydeo.fintracker.service.ReportingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.Month;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -30,64 +22,35 @@ import java.util.List;
 @Slf4j
 public class ReportingServiceImpl implements ReportingService {
 
-    private final SecurityService securityService;
+
     private final InvoiceProductService invoiceProductService;
     private final CompanyService companyService;
-    private final InvoiceService invoiceService;
-    private final MapperUtil mapperUtil;
+    private final ReportRepository reportRepository;
 
 
     @Override
     public Map<String, BigDecimal> getMonthlyProfitLoss() {
-
-        Map<String, BigDecimal> mapOfDateAndProfitLoss = new HashMap<>();
         CompanyDto company = companyService.getCompanyDtoByLoggedInUser().get(0);
-
         Long companyId = company.getId();
-        LocalDateTime companyInsertDate = company.getInsertDate();
 
-        List<LocalDate> dateKeys = listDate(companyInsertDate);
+        List<Object[]> results = reportRepository.findReportsByCompanyIdAndInvoiceType(companyId, InvoiceType.SALES);
 
-        for (LocalDate date : dateKeys) {
+        Map<String, BigDecimal> reportData = new HashMap<>();
 
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy MMMM");
-            String key = date.format(formatter);
-
-            BigDecimal profitLoss = invoiceProductService.getProfitLossBasedOnMonth
-                    (date.getYear(), date.getMonthValue(), companyId, InvoiceType.SALES);
-            mapOfDateAndProfitLoss.put(key.toUpperCase(), profitLoss);
-
+        for (Object[] result : results) {
+            int year = (int) result[0];
+            int month = (int) result[1];
+            String period = year + " " + Month.of(month).toString();
+            BigDecimal totalProfitLoss = (BigDecimal) result[2];
+            reportData.put(period, totalProfitLoss);
         }
+        log.info("Monthly profit/loss data has been retrieved successfully for company with ID {}. Total {} data points retrieved for the period.", companyId, reportData.size());
 
-        log.info("Monthly profit/loss data has been retrieved for company with ID {}.", companyId);
-
-        return mapOfDateAndProfitLoss;
-    }
-
-    public List<LocalDate> listDate(LocalDateTime localDateTime) {
-        if (localDateTime == null) {
-            throw new IllegalArgumentException("localDateTime cannot be null");
-        }
-
-        int year = localDateTime.getYear();
-        Month month = localDateTime.getMonth();
-        int day = localDateTime.getDayOfMonth();
-
-        LocalDate startDate = LocalDate.of(year, month, day);
-        LocalDate now = LocalDate.now();
-
-        List<LocalDate> listOfDate = new ArrayList<>();
-
-        while (startDate.isBefore(now)) {
-            listOfDate.add(startDate);
-            startDate = startDate.plusMonths(1);
-
-        }
-        log.info("list of months from company insert date till now: {} ", listOfDate);
-        return listOfDate;
+        return reportData;
     }
 
 
+    @Override
     public List<InvoiceProductDto> generateStockReport() {
 
         return invoiceProductService.findAllApprovedInvoiceProducts(InvoiceStatus.APPROVED);
